6580b75478f35e533fc10da8e07510b3c4db3476
Quvonchbek0203 <quvonchbeknormurodov62@gmail.com>
2024-04-14 21:50:33 +0500

    button
    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <div>
-      <button></button>
+      <button className='w-[100px] h-10 rounded-3xl bg-white text-lime-500 text-lg font-bold'>Help Me</button>
     </div>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import mainImg from "../../images/main.png"
 import TitleText from '../TitleText'
+import Logo from '../../images/logo round.png'
+import Button from '../Button'
 
 
 const Header = () => {
@@ -12,10 +14,26 @@ const Header = () => {
     }
   return (
     <div className='w-full h-screen' style={backImage}>
-        <div className='container'>
-            <TitleText title="Service"/>
+        {/* <div className='container w-full h-full'> */}
+        <div className='flex justify-center items-center flex-wrap gap-5'>
+        <div className='flex justify-center items-center flex-wrap text-white w-full'>
+          <div className='w-20 '><img src={Logo} alt="logo"/></div>
+            <div>
+            <h1 className='font-bold text-4xl'>Round Rock</h1>
+            <p className='font-bold text-2xl'>Yard Maintenance</p>
+            </div>
+          </div>
+          <button className='text-white w-60 h-10 bg-transparent bg- '>Lorem ipsum dolor sit amet</button>        
         </div>
-    </div>
+        <div className='flex flex-wrap'>
+        <div><h1 className='text-8xl font-bold text-white text-center w-[800px] h-[100px]'>WELCOME TO ROUND ROCK COMPANY</h1></div>
+        <div>
+          <Button/>
+        </div>
+
+        </div>
+        </div>
+    // </div>
   )
 }
 
