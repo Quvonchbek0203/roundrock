39508637913122e94126769a1e9519a3818eda0e
Quvonchbek0203 <quvonchbeknormurodov62@gmail.com>
2024-04-25 17:14:38 +0500

    SERVISE
    SERVISE

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import mainImg from "../../images/main.png"
-import Logo from '../../images/logo round.png'
+import Logo from '../../images/logo.jpeg'
 import Button from '../Button'
+
+
 
 
 const Header = () => {
@@ -14,18 +16,18 @@ const Header = () => {
   return (
     <div className='w-full h-screen' style={backImage}>
         <div className='flex justify-center items-center flex-wrap'>
-        <div className='flex justify-center items-center flex-wrap text-white w-full'>
-          <div className='w-20 '><img src={Logo} alt="logo"/></div>
+        <div className='flex justify-center items-center flex-wrap gap-4 text-white w-full'>
+          <div className='w-20 h-10'><img src={Logo} alt="logo" className='rounded-3xl'/></div>
             <div>
-            <h1 className='font-bold text-4xl'>Round Rock</h1>
-            <p className='font-bold text-2xl'>Yard Maintenance</p>
+            <h1 className='font-bold text-4xl'>I-MAX</h1>
+            <p className='font-bold text-2xl'>Doimo siz bilan</p>
             </div>
           </div>
           <div className='w-full h-20 flex justify-center items-center'>
-            <button className='text-white text-center rounded-3xl w-80 h-10 bg-opacity-40 bg-black'>Lorem ipsum dolor sit amet</button>
+            <button className='text-white text-center rounded-3xl w-72 h-10 bg-opacity-40 bg-black'>Biz bilan bog'lanish</button>
           </div>
           <div className='w-full flex justify-center '>
-            <h1 className='w-[800px] text-center font-bold text-8xl text-green-950'>Welcome to ROUND ROCK Company</h1>
+            <h1 className='w-[800px] text-center font-bold text-8xl text-green-950'>Welcome to <h1 className='font-bolt text-9xl'>I-MAX</h1> Company</h1>
           </div>
           <div className='w-full flex justify-center items-center h-10 absolute bottom-10'>
             <Button/>
--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
 import Vektor from '../../images/Vector.png'
+import Ucell from '../../images/ucell.jpg'
+import Beeline from '../../images/beeline.jpg'
+import Mobi from '../../images/mobiuz.png' 
+import Uzmobile from '../../images/uzmobile.png'
+
 
 const Service = () => {
   return (
-    <div className='flex relative justify-center items-center flex-wrap'>
+    <div className='flex relative justify-center items-center flex-wrap gap-20'>
+      <div>
       <h1 className='w-full text-center text-5xl font-bold text-gray-500'>Services</h1>
-      <div className='w-16 p-l-100'>
-        <img src={Vektor} alt="Vektor" />
+      <div className='w-full relative'>
+        <img src={Vektor} alt="Vektor" className='w-20 absolute left-[-10px] top-[-15px]'/>
+      </div>
+      </div>
+      <div className='w-full flex justify-center items-center gap-4'>
+        <div className='w-[200px]'>
+          <div className='p-2'>
+            <img src={Ucell} alt="Ucell" className='w-[200px] rounded-3xl' />
+          </div>
+          <div className='w-[200px] flex justify-center items-center flex-wrap gap-2'>
+            <h1 className='font-bold text-lime-900'>UCELL</h1>
+            <p className='text-xs text-lime-900 w-50 text-left'>Hozirgi kunda kompaniyada 1250 ta xodim ishlaydi. Abonent bazasi esa 7.1 millionni tashkil etadi. 2G tarmog‘i O‘zbekiston hududini 97% ni qoplaydi, 3G tarmog‘i esa – 60%.</p>
+          </div>
+        </div>
+        <div className='w-[200px]'>
+          <div className='p-2 h-[200px] flex justify-center items-center'>
+          <img src={Beeline} alt="Beeline" className='w-[220px] rounded-3xl h-[150PX]' />
+          </div>
+          <div className='w-[200px] flex justify-center items-center flex-wrap gap-2'>
+            <h1 className='font-bold text-lime-900'>BEELINE</h1>
+            <p className='text-xs text-lime-900 w-50 text-left'>Beeline ( 2005-yilgacha "Beeline GSM") — VimpelCom Ltd.shuning ichida telekommunikatsiya sohasida (birinchi navbatda mobil aloqa) Rossiya va MDH davlatlari oʻrtasidagi yetakchi oʻrin oladi.</p>
+          </div>
+        </div>
+        <div className='w-[200px]'>
+          <div className='p-2'>
+            <img src={Mobi} alt="Mobi " className='w-[200px] rounded-3xl' />
+          </div>
+          <div className='w-[200px] flex justify-center items-center flex-wrap gap-2'>
+            <h1 className='font-bold text-lime-900'>MobiUZ</h1>
+            <p className='text-xs text-lime-900 w-50 text-left'>Kompaniya 2014-yil 1-dekabrdan O‘zbekiston mobil aloqa bozorida faoliyat yuritib kelmoqda.Mobiuz ta’sischisi - O‘zbekiston Respublikasi Raqamli texnologiyalar vazirligidir</p>
+          </div>
+        </div>
+        <div className='w-[200px]'>
+          <div className='p-2'>
+            <img src={Uzmobile} alt="Uzmobile" className='w-[250px] rounded-3xl h-100' />
+          </div>
+          <div className='w-[250px] flex justify-center items-center flex-wrap gap-4'>
+            <h1 className='font-bold text-lime-900'>Uzmobile</h1>
+            <p className='text-xs text-lime-900 w-50 text-left'>Beeline (shuningdek: Beeline, 2005-yilgacha "Beeline GSM") — VimpelCom Ltd.shuning ichida telekommunikatsiya sohasida (birinchi navbatda mobil aloqa) Rossiya va MDH davlatlari oʻrtasidagi yetakchi oʻrin oladi.</p>
+          </div>
+        </div>
       </div>
     </div>
   )
